d15bb2ff3d61ade2c5d0daca4c893308245ce93e
Tu Nguyen <tunguyen0930@gmail.com>
2020-07-04 18:21:41 +0700

    App - init

---
+++ Assingment2/Assingment2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assingment2
+//
+//  Created by Nguyen Anh Tu on 7/4/20.
+//  Copyright © 2020 Nguyen Anh Tu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
