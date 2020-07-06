8704540ed5b71bae52aa2057183693083db1f37b
tuna <tuna@minerva.vn>
2020-07-06 11:58:09 +0700

    Create list

---
+++ Assingment2/Assingment2/ListViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ListViewController.swift
+//  Assingment2
+//
+//  Created by Minerva on 7/6/20.
+//  Copyright © 2020 Nguyen Anh Tu. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    
+    @IBOutlet weak var tableview: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableview.delegate = self
+        tableview.dataSource = self
+    }
+
+
+}
+
+extension ListViewController: UITableViewDelegate {
+    
+}
+
+extension ListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+    
+    
+}
